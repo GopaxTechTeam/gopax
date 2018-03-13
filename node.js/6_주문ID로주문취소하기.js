32e93934ec0ddc5d3be70844d9d02f0b455f9a77
gopaxapi <36653428+gopaxapi@users.noreply.github.com>
2018-03-13 15:49:54 +0900

    Create 6_주문ID로주문취소하기.js

---
+++ node.js/6_주문ID로주문취소하기.js
@@ -0,0 +1,47 @@
+//
+// 주문 ID로 주문 취소하기
+//
+var crypto = require('crypto');
+var request = require('request');
+
+// 발급받은 api키와 시크릿키를 입력한다
+var apikey = '';
+var secret = '';
+
+// nonce값 생성
+var nonce = Date.now() * 1000;
+var method = 'DELETE';
+var requestPath = '/orders/66233372'; // /orders/<order-id>
+
+// 필수 정보를 연결하여 prehash 문자열을 생성함
+var what = nonce + method + requestPath;
+// base64로 secret을 디코딩함
+var key = Buffer(secret, 'base64');
+// secret으로 sha512 hmac을 생성함
+var hmac = crypto.createHmac('sha512', key);
+
+// hmac으로 필수 메시지에 서명하고
+// 그 결과물을 base64로 인코딩함
+var sign = hmac.update(what).digest('base64');
+
+var host = 'api.gopax.co.kr';
+
+var options = {
+  method,
+  json: true,
+  url: `https://${host}${requestPath}`,
+  headers: {
+    'API-KEY': apikey,
+    Signature: sign,
+    Nonce: nonce
+  },
+  strictSSL: false,
+};
+
+request(options, (err, response, b) => {
+  if (err) {
+    console.log('err:', err);
+    return;
+  }
+  console.log(b);
+});
